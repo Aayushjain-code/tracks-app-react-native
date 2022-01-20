58274a5fba949a1d22b36a50d27a93c213a6b85d
Aayus Jain <aayushjain6020@gmail.com>
2022-01-20 09:54:59 +0530

    fetching created tracks

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export default axios.create({
-  baseURL: "http://c1a1-49-36-41-238.ngrok.io",
+const instance = axios.create({
+  baseURL: "http://da42604cecbb.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
